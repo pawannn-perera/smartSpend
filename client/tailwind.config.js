50346171055f503b0a1c1b439725f904dcffe9c9
Pawan Perera <pawanperera5@gmail.com>
2025-06-07 09:28:59 +0530

    update

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Roboto", "sans-serif"],
+        sans: ["Poppins", "sans-serif"],
       },
       colors: {
         "dark-blue": "#1f2937",
