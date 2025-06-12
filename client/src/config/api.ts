4fbfb5f6de14ffd2cdc343c5d1b1c08cf4bdc0ef
Pawan Perera <pawanperera5@gmail.com>
2025-06-12 10:03:48 +0530

    update

--- client/src/config/api.ts
+++
@@ -1,7 +0,0 @@
-// src/config/api.ts
-const API_BASE_URL: string =
-  process.env.NODE_ENV === "production"
-    ? "https://smartspend-server.vercel.app"
-    : "http://localhost:5000";
-
-export default API_BASE_URL;
